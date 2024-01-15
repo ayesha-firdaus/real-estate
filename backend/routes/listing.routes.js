9efcec4373479cba4b753cb65d9e082ac41ce10f
ayesha <ayeshafirdaus21@gmail.com>
2024-01-15 14:04:06 +0530

    image

--- backend/routes/listing.routes.js
+++ backend/routes/listing.routes.js
@@ -5,6 +5,7 @@ const router=express.Router();
 router.post('/create',verifyUser,createListing);
 router.delete('/delete/:id',verifyUser,deletelist)
 router.patch("/update/:id",verifyUser,updatelist);
+
 router.get('/get/:id',getlist)
 // router.delete('/delete/:id',verifyUser,deleteListing);
 module.exports=router;
