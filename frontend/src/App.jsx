f5ea42df27414d58f8bcb78802fe06317d05fc84
ayesha <ayeshafirdaus21@gmail.com>
2024-01-18 11:37:52 +0530

    search page ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
 import Listing from "./pages/Listing";
+import Search from "./pages/Search";
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
      <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/sign-in" element={<Signin />} />
+      <Route path="/search" element={<Search />} />
       <Route element={<PrivateRoute />}>
       <Route path="/profile" element={<Profile />}/>
       <Route path="/create-listing"  element={<CreateListing/>} />
